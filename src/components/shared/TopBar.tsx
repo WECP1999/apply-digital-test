0320163d59d8c792b001f62b8f73847067ea7f11
WECP1999 <wecp123@gmail.com>
2025-02-13 20:34:45 -0600

    style: change topbar style
    Add padding to the TopBar component and change button style

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -4,18 +4,18 @@ import { RiShoppingCart2Line } from '@remixicon/react';
 
 const TopBar = () => {
   return (
-    <nav className="py-5 w-full bg-primary-200">
+    <nav className="py-5 px-6 max-2xl:px-0 w-full bg-primary-200">
       <div className="max-w-screen-2xl w-full flex flex-row justify-between items-center mx-auto">
         <Link href="/">
           <Button
             buttonType="text"
-            className="font-bold text-2xl"
+            className="font-bold !text-2xl !px-0 !min-w-0 !border-none"
           >
             GamerShop
           </Button>
         </Link>
         <Link href="/shopping-cart">
-          <RiShoppingCart2Line className="text-primary-400" />
+          <RiShoppingCart2Line className="text-primary-400 text-xl" />
         </Link>
       </div>
     </nav>
