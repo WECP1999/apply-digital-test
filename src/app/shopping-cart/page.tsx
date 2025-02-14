8d92a43de9e0b5834456898d6c9539c916c75b01
WECP1999 <wecp123@gmail.com>
2025-02-13 20:39:04 -0600

    feat: add shopping cart page
    Add shopping cart page with the order summary component

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -1,5 +1,41 @@
+'use client';
+
+import OrderSummary from '@/components/orderSummary/OrderSummary';
+import Button from '@/components/shared/Button';
+import cartStore from '@/store/cart';
+import { RiArrowLeftLine } from '@remixicon/react';
+import Link from 'next/link';
+
 const ShoppingCart = () => {
-  return <section className=""></section>;
+  const games = cartStore((state) => state.games);
+
+  return (
+    <section className="flex min-h-screen flex-col px-24 font-bold text-4xl text-blue-600 max-w-screen-2xl m-auto w-full box-border">
+      <section className="flex flex-col gap-12 py-12">
+        <div className="py-6">
+          <Link
+            href="/"
+            className="flex flex-row gap-2 text-base text-primary-500 font-medium"
+          >
+            <RiArrowLeftLine />
+            <span className="">Back to Catalog</span>
+          </Link>
+        </div>
+        <div className="flex flex-col gap-3">
+          <span className="font-bold text-4xl text-primary-500">
+            Top Sellers
+          </span>
+          <span className="text-primary-500 text-2xl">
+            {games.length} items
+          </span>
+        </div>
+      </section>
+      <section className="flex flex-col justify-between md:flex-row">
+        <div className=""></div>
+        <OrderSummary games={games} />
+      </section>
+    </section>
+  );
 };
 
 export default ShoppingCart;
