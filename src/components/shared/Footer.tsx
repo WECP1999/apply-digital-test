ce064a16f1b7a274599a94cd03c5fab134deafd7
WECP1999 <wecp123@gmail.com>
2025-02-14 11:38:28 -0600

    feat: add link to footer
    Add link to footer apply digital logo

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,20 +2,23 @@ import Image from 'next/image';
 import ApplyLogo from '@/../public/footer/ApplyLogo.png';
 import Apply from '../icons/Apply';
 import Digital from '../icons/Digital';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className="bg-neutral-700 w-full py-16">
-      <div className="flex flex-row gap-3.5 items-center justify-center">
-        <Image
-          src={ApplyLogo}
-          alt="Apply Digital Logo"
-        />
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <Apply />
-          <Digital />
+      <Link href="/">
+        <div className="flex flex-row gap-3.5 items-center justify-center">
+          <Image
+            src={ApplyLogo}
+            alt="Apply Digital Logo"
+          />
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <Apply />
+            <Digital />
+          </div>
         </div>
-      </div>
+      </Link>
     </footer>
   );
 };
