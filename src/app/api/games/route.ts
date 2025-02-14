8ec01a9cd695c9fb37a8c6e31f1aba981afbc333
WECP1999 <wecp123@gmail.com>
2025-02-13 20:27:36 -0600

    feat: update game endpoint
    Update the way total pages was calculated

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,11 +1,11 @@
-import { allGames, availableFilters, delay } from "@/utils/endpoint";
+import { allGames, availableFilters, delay } from '@/utils/endpoint';
 
 const ITEMS_PER_PAGE = 12;
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const genre = searchParams.get("genre");
-  let page = parseInt(searchParams.get("page") ?? "1");
+  const genre = searchParams.get('genre');
+  let page = parseInt(searchParams.get('page') ?? '1');
 
   let games = allGames;
 
@@ -22,9 +22,10 @@ export async function GET(request: Request) {
 
   const fromIndex = (page - 1) * ITEMS_PER_PAGE;
   const toIndex = page * ITEMS_PER_PAGE;
+  const totalPages = Math.ceil(games.length / ITEMS_PER_PAGE);
+
   games = games.slice(fromIndex, toIndex);
 
-  const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
   const currentPage = page;
 
   return Response.json({ games, availableFilters, totalPages, currentPage });
